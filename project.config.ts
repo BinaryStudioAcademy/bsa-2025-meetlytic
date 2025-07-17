ca8f3b6b4ce4fd2801d41380353e3aa0f1cf7552
Farid Shabanov <farid.shabanov@binary-studio.com>
2025-07-17 13:26:04 +0400

    feat: add starter ml-1

---
+++ project.config.ts
@@ -0,0 +1,24 @@
+const ProjectPrefix = {
+	APP: "ml",
+	CHANGE_TYPES: [
+		"build",
+		"chore",
+		"ci",
+		"docs",
+		"feat",
+		"fix",
+		"perf",
+		"refactor",
+		"revert",
+		"style",
+		"test",
+	],
+	ENVIRONMENT: "main",
+	ISSUE_PREFIXES: ["ml"],
+	SCOPES: {
+		APPS: ["frontend", "backend"],
+		PACKAGES: ["shared"],
+	},
+} as const;
+
+export { ProjectPrefix };

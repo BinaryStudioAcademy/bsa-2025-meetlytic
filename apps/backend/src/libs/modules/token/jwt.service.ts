a16fbfa9851c820fe02b62d57bb5fcc0a10c6eef
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-07-19 18:48:04 +0300

    feat: refact jwt service ml-9

--- apps/backend/src/libs/modules/token/jwt.service.ts
+++ apps/backend/src/libs/modules/token/jwt.service.ts
@@ -1,22 +1,24 @@
-import { BaseToken } from "./base-token.js";
+import { type BaseToken } from "./base-token.js";
 import { type JwtPayload } from "./libs/types/types.js";
 
-const SECRET = new TextEncoder().encode(process.env["JWT_SECRET"]);
-const EXPIRES_IN = process.env["JWT_EXPIRES_IN"] ?? "24h";
-const ALGORITHM = process.env["JWT_ALGORITHM"] ?? "HS256";
-
-const baseToken = new BaseToken<JwtPayload>({
-	algorithm: ALGORITHM,
-	secret: SECRET,
-});
-
 class JwtService {
+	private baseToken: BaseToken<JwtPayload>;
+	private expiresIn: string;
+
+	public constructor(baseToken: BaseToken<JwtPayload>, expiresIn: string) {
+		this.baseToken = baseToken;
+		this.expiresIn = expiresIn;
+	}
+
 	public async sign(payload: JwtPayload): Promise<string> {
-		return await baseToken.createToken({ expirationTime: EXPIRES_IN, payload });
+		return await this.baseToken.createToken({
+			expirationTime: this.expiresIn,
+			payload,
+		});
 	}
 
 	public async verify(token: string): Promise<JwtPayload> {
-		const { payload } = await baseToken.decode(token);
+		const { payload } = await this.baseToken.decode(token);
 		return payload;
 	}
 }
--- apps/backend/src/libs/modules/token/jwt.ts
+++ apps/backend/src/libs/modules/token/jwt.ts
@@ -1,5 +1,18 @@
-import { JwtService } from "./jwt.service.js";
+import { config } from "~/libs/modules/config/config.js";
 
-const jwt = new JwtService();
+import { BaseToken } from "./base-token.js";
+import { JwtService } from "./jwt.service.js";
+import { type JwtPayload } from "./libs/types/types.js";
+
+const SECRET = new TextEncoder().encode(config.ENV.TOKEN.SECRET);
+const ALGORITHM = config.ENV.TOKEN.ALGORITHM;
+const EXPIRES_IN = config.ENV.TOKEN.EXPIRES_IN;
+
+const baseToken = new BaseToken<JwtPayload>({
+	algorithm: ALGORITHM,
+	secret: SECRET,
+});
+
+const jwt = new JwtService(baseToken, EXPIRES_IN);
 
 export { jwt };

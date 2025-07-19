70b36f6beeb4d07b79147186e8b7416e4670c9d9
Volodymyr <Volodumurvel@gmail.com>
2025-07-19 19:45:49 +0200

    refactor: add  to APIHandlerOptions for authenticated routes  ml-15

--- apps/backend/src/libs/modules/controller/libs/types/api-handler-options.type.ts
+++ apps/backend/src/libs/modules/controller/libs/types/api-handler-options.type.ts
@@ -4,12 +4,14 @@ type APIHandlerOptions<
 	body: T["body"];
 	params: T["params"];
 	query: T["query"];
+	user: T["user"];
 };
 
 type DefaultApiHandlerOptions = {
 	body?: unknown;
 	params?: unknown;
 	query?: unknown;
+	user?: unknown;
 };
 
 export { type APIHandlerOptions };

a0342ba641c046b331e97f05f3acb45cbd7d1db6
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-19 21:40:09 +0300

    feat: add user attribute enum and type ml-5

--- apps/backend/src/modules/users/libs/types/types.ts
+++ apps/backend/src/modules/users/libs/types/types.ts
@@ -1,5 +1,9 @@
+export { type UserAttribute } from "./user-attribute.type.js";
+
 export {
 	type UserGetAllResponseDto,
+	type UserSignInRequestDto,
+	type UserSignInResponseDto,
 	type UserSignUpRequestDto,
 	type UserSignUpResponseDto,
 } from "@meetlytic/shared";

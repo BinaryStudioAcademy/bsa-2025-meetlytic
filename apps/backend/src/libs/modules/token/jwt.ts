92f3d8b4ddbbf97d0a2f24ab3d197e2bdf3e3cf9
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-07-20 10:03:15 +0300

    fix: authorization token ml-9

--- apps/backend/src/libs/modules/token/jwt.ts
+++ apps/backend/src/libs/modules/token/jwt.ts
@@ -1,18 +1,12 @@
 import { config } from "~/libs/modules/config/config.js";
 
-import { BaseToken } from "./base-token.js";
-import { JwtService } from "./jwt.service.js";
+import { BaseToken } from "./base-token.module.js";
 import { type JwtPayload } from "./libs/types/types.js";
 
-const SECRET = new TextEncoder().encode(config.ENV.TOKEN.SECRET);
-const ALGORITHM = config.ENV.TOKEN.ALGORITHM;
-const EXPIRES_IN = config.ENV.TOKEN.EXPIRES_IN;
-
-const baseToken = new BaseToken<JwtPayload>({
-	algorithm: ALGORITHM,
-	secret: SECRET,
+const jwt = new BaseToken<JwtPayload>({
+	algorithm: config.ENV.TOKEN.ALGORITHM,
+	expirationTime: config.ENV.TOKEN.EXPIRES_IN,
+	secret: new TextEncoder().encode(config.ENV.TOKEN.SECRET),
 });
-
-const jwt = new JwtService(baseToken, EXPIRES_IN);
 
 export { jwt };

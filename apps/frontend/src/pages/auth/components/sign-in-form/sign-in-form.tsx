2e22eb04ef959b9f1935ffd173b4505a9bc84b3a
Anastasiia Bielik <ana.stasy0112@gmail.com>
2025-07-22 00:23:23 +0300

    feat: add sign-in styles ml-6

--- apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,16 +1,71 @@
-import { Button } from "~/libs/components/components.js";
+import { useForm } from "react-hook-form";
 
-type Properties = {
-	onSubmit: () => void;
+import { Button, Input } from "~/libs/components/components.js";
+import { AuthLayout } from "~/pages/auth/components/auth-layout/auth-layout.js";
+
+import "./sign-in-form.css";
+
+type FormValues = {
+	email: string;
+	password: string;
 };
 
-const SignInForm: React.FC<Properties> = () => (
-	<>
-		<h1>Sign In</h1>
-		<form>
-			<Button label="Sign in" />
-		</form>
-	</>
-);
+type Properties = {
+	onSubmit: (data: FormValues) => void;
+};
+
+const SignInForm: React.FC<Properties> = ({ onSubmit }) => {
+	const {
+		control,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<FormValues>({
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const submitHandler = (data: FormValues) => {
+		onSubmit(data);
+	};
+
+	return (
+		<AuthLayout>
+			<h2 className="title">Login to your Account</h2>
+
+			<form className="login-form" onSubmit={handleSubmit(submitHandler)}>
+				<p>
+					<Input
+						control={control}
+						errors={errors}
+						label="Email"
+						name="email"
+						placeholder="mail@abc.com"
+						type="text"
+					/>
+				</p>
+				<p>
+					<Input
+						control={control}
+						errors={errors}
+						label="Password"
+						name="password"
+						placeholder="********"
+						type="text"
+					/>
+				</p>
+				<Button className="sign-in-btn" label="Login" type="submit" />
+			</form>
+
+			<div className="register">
+				Not Registered Yet?{" "}
+				<a className="create-account" href="/sign-up">
+					Create an account
+				</a>
+			</div>
+		</AuthLayout>
+	);
+};
 
 export { SignInForm };

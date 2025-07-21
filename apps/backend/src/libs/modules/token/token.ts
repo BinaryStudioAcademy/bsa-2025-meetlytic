c686d97be4c6ebc7d648ac0c040413d03c688c47
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-07-22 00:37:43 +0300

    refactor: add env vars into token.ts ml-9

--- apps/backend/src/libs/modules/token/token.ts
+++ apps/backend/src/libs/modules/token/token.ts
@@ -3,10 +3,12 @@ import { config } from "~/libs/modules/config/config.js";
 import { BaseToken } from "./base-token.module.js";
 import { type JwtPayload } from "./libs/types/types.js";
 
+const { ALGORITHM, EXPIRES_IN, SECRET } = config.ENV.TOKEN;
+
 const jwt = new BaseToken<JwtPayload>({
-	algorithm: config.ENV.TOKEN.ALGORITHM,
-	expirationTime: config.ENV.TOKEN.EXPIRES_IN,
-	secret: new TextEncoder().encode(config.ENV.TOKEN.SECRET),
+	algorithm: ALGORITHM,
+	expirationTime: EXPIRES_IN,
+	secret: new TextEncoder().encode(SECRET),
 });
 
 export { jwt };

cd6efcd33199552d6784679d01db667667256d3d
oksana <ok-ksenia@ukr.net>
2025-07-21 14:56:09 +0300

    feat: add migration for user_details table schema ml-7

---
+++ apps/backend/src/db/migrations/20240127205705_add_user_details_table.ts
@@ -0,0 +1,41 @@
+import { type Knex } from "knex";
+
+const TABLE_NAME = "user_details";
+
+const ColumnName = {
+	CREATED_AT: "created_at",
+	FIRST_NAME: "first_name",
+	ID: "id",
+	LAST_NAME: "last_name",
+	UPDATED_AT: "updated_at",
+	USER_ID: "user_id",
+} as const;
+
+function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable(TABLE_NAME, (table) => {
+		table.increments(ColumnName.ID).primary();
+		table.string(ColumnName.FIRST_NAME).notNullable();
+		table.string(ColumnName.LAST_NAME).notNullable();
+		table
+			.integer(ColumnName.USER_ID)
+			.unsigned()
+			.notNullable()
+			.references("id")
+			.inTable("users")
+			.onDelete("CASCADE");
+		table
+			.dateTime(ColumnName.CREATED_AT)
+			.notNullable()
+			.defaultTo(knex.fn.now());
+		table
+			.dateTime(ColumnName.UPDATED_AT)
+			.notNullable()
+			.defaultTo(knex.fn.now());
+	});
+}
+
+export { down, up };

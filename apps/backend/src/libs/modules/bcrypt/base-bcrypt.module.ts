c7c1db83d9af9e7abf020b3dce0d3ee64a5a177c
oksana <ok-ksenia@ukr.net>
2025-07-22 17:26:51 +0300

    feat: add bcrypt, update package.json package-lock.json ml-7

---
+++ apps/backend/src/libs/modules/bcrypt/base-bcrypt.module.ts
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+
+class BaseBcrypt {
+	async hash(password: string): Promise<{ hash: string; salt: string }> {
+		const salt = await bcrypt.genSalt();
+		const hash = await bcrypt.hash(password, salt);
+
+		return { hash, salt };
+	}
+
+	async verify(
+		plainPassword: string,
+		hashedPassword: string,
+	): Promise<boolean> {
+		return await bcrypt.compare(plainPassword, hashedPassword);
+	}
+}
+
+export { BaseBcrypt };

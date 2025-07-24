dead1524dcc26114789f33ed8d15fed075d68a39
Roman-PRV <prokopenkorv@gmail.com>
2025-07-24 13:14:08 +0200

    refactor: standardize React type usage ml-11

--- apps/frontend/src/libs/components/router-provider/protected-route.tsx
+++ apps/frontend/src/libs/components/router-provider/protected-route.tsx
@@ -1,16 +1,17 @@
-import { FC, ReactNode } from "react";
-
 import { Navigate } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { useAppSelector, useLocation } from "~/libs/hooks/hooks.js";
 import { ValueOf } from "~/libs/types/types.js";
 
 type Properties = {
-	children: ReactNode;
+	children: React.ReactNode;
 	redirectTo: ValueOf<typeof AppRoute>;
 };
 
-const ProtectedRoute: FC<Properties> = ({ children, redirectTo }) => {
+const ProtectedRoute: React.FC<Properties> = ({
+	children,
+	redirectTo,
+}: Properties) => {
 	const isAuthenticated = useAppSelector((state) => state.auth.user !== null);
 	const location = useLocation();
 

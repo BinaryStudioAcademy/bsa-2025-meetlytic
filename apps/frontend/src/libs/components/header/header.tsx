42a0bb1fffe7bc7bb54e6a272cb83dd67caec5d4
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-24 11:05:02 +0300

    refactor: remove getValidClassNames ml-16

--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -1,12 +1,11 @@
 import { Avatar, Logo } from "~/libs/components/components.js";
-import { getValidClassNames } from "~/libs/helpers/helpers.js";
 
 import styles from "./styles.module.css";
 
 const Header: React.FC = () => {
 	return (
-		<header className={getValidClassNames(styles["header"])}>
-			<div className={getValidClassNames(styles["headerInner"])}>
+		<header className={styles["header"]}>
+			<div className={styles["headerInner"]}>
 				<Logo />
 				<Avatar />
 			</div>
--- apps/frontend/src/libs/components/logo/logo.tsx
+++ apps/frontend/src/libs/components/logo/logo.tsx
@@ -1,20 +1,16 @@
 import { Link } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
-import { getValidClassNames } from "~/libs/helpers/helpers.js";
 
 import styles from "./styles.module.css";
 
 type Properties = {
-	withLink?: boolean;
+	hasLink?: boolean;
 };
 
-const Logo: React.FC<Properties> = ({ withLink = true }: Properties) => {
-	const logoContent = (
-		<span className={getValidClassNames(styles["logoText"])}>LOGO</span>
-	);
-
-	return withLink ? (
-		<Link className={getValidClassNames(styles["logoLink"])} to={AppRoute.ROOT}>
+const Logo: React.FC<Properties> = ({ hasLink = true }: Properties) => {
+	const logoContent = <span className={styles["logoText"]}>Meetlytic</span>;
+	return hasLink ? (
+		<Link className={styles["logoLink"]} to={AppRoute.ROOT}>
 			{logoContent}
 		</Link>
 	) : (

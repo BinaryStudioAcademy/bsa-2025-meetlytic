3858106abab32653efd45cf8df0ededb6ebc0ca7
Anastasiia Bielik <ana.stasy0112@gmail.com>
2025-07-24 14:02:56 +0300

    refactor: updated styles to CSS Modules syntax ml-6

--- apps/frontend/src/pages/auth/components/auth-layout/auth-layout.tsx
+++ apps/frontend/src/pages/auth/components/auth-layout/auth-layout.tsx
@@ -7,15 +7,15 @@ interface AuthLayoutProperties {
 const AuthLayout = ({ children }: AuthLayoutProperties) => {
 	return (
 		<div className={styles["auth"]}>
-			<div className={styles["auth__left"]}>
-				<div className={styles["auth__logo"]}>
-					<span className={styles["auth__logo-circle"]} />
-					<span className={styles["auth__logo-text"]}>LOGO</span>
+			<div className={styles["authLeft"]}>
+				<div className={styles["authLogo"]}>
+					<span className={styles["authLogoCircle"]} />
+					<span className={styles["authLogoText"]}>LOGO</span>
 				</div>
 			</div>
 
-			<div className={styles["auth__right"]}>
-				<div className={styles["auth__form-wrapper"]}>{children}</div>
+			<div className={styles["authRight"]}>
+				<div className={styles["authFormWrapper"]}>{children}</div>
 			</div>
 		</div>
 	);

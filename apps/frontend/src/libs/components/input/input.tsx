6477e37bdbddfec33324dd3ebef13e35421c38f3
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-24 20:39:05 +0300

    feat: add reusable input component ml-40

--- apps/frontend/src/libs/components/input/input.tsx
+++ apps/frontend/src/libs/components/input/input.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
 	type Control,
 	type FieldErrors,
@@ -6,20 +5,36 @@ import {
 	type FieldValues,
 } from "react-hook-form";
 
+import { Icon } from "~/libs/components/components.js";
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import { useFormController } from "~/libs/hooks/hooks.js";
+import { type IconName } from "~/libs/types/types.js";
+
+import { iconPositionToClass } from "./libs/maps/icon-position-to-class.map.js";
+import styles from "./styles.module.css";
 
 type Properties<T extends FieldValues> = {
+	className?: string;
 	control: Control<T, null>;
 	errors: FieldErrors<T>;
+	hasLabel?: boolean;
+	iconClassName?: string;
+	iconName?: "" | IconName;
+	iconPosition?: "" | "left" | "right";
 	label: string;
 	name: FieldPath<T>;
 	placeholder?: string;
-	type?: "email" | "text";
+	type?: React.HTMLInputTypeAttribute;
 };
 
 const Input = <T extends FieldValues>({
+	className = "",
 	control,
 	errors,
+	hasLabel = true,
+	iconClassName = "",
+	iconName = "",
+	iconPosition = "left",
 	label,
 	name,
 	placeholder = "",
@@ -29,11 +44,33 @@ const Input = <T extends FieldValues>({
 
 	const error = errors[name]?.message;
 	const hasError = Boolean(error);
+	const isIconPadded = iconName && iconPosition === "left";
 
 	return (
-		<label>
-			<span>{label}</span>
-			<input {...field} placeholder={placeholder} type={type} />
+		<label className={styles["lable-wrapper"]}>
+			{hasLabel && <span>{label}</span>}
+			<span className={styles["input-relative-wrapper"]}>
+				{iconName && (
+					<Icon
+						className={getValidClassNames(
+							styles["input-icon"],
+							iconClassName,
+							iconPosition && iconPositionToClass[iconPosition],
+						)}
+						name={iconName}
+					/>
+				)}
+				<input
+					className={getValidClassNames(
+						className,
+						styles["input"],
+						isIconPadded && styles["input--left-padding"],
+					)}
+					{...field}
+					placeholder={placeholder}
+					type={type}
+				/>
+			</span>
 			{hasError && <span>{error as string}</span>}
 		</label>
 	);
---
+++ apps/frontend/src/libs/components/input/libs/maps/icon-position-to-class.map.ts
@@ -0,0 +1,8 @@
+import styles from "../../styles.module.css";
+
+const iconPositionToClass = {
+	left: styles["input-icon--left"],
+	right: styles["input-icon--right"],
+};
+
+export { iconPositionToClass };

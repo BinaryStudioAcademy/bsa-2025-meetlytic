64cf8accc30a6494c1ff6a36e08796aaaa9db1ee
Egor Kostiuk <kostukegor729@gmail.com>
2025-07-24 11:28:52 +0300

    feat: add reusable button component ml-39

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,11 +1,37 @@
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
+
+import styles from "./styles.module.css";
+
 type Properties = {
+	disabled?: boolean;
 	label: string;
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	size?: "default" | "small";
 	type?: "button" | "submit";
+	variant?: "outlined" | "primary";
 };
 
 const Button: React.FC<Properties> = ({
+	disabled = false,
 	label,
+	onClick,
+	size = "default",
 	type = "button",
-}: Properties) => <button type={type}>{label}</button>;
+	variant = "primary",
+}: Properties) => (
+	<button
+		className={getValidClassNames(
+			styles["button"],
+			styles[`button--${variant}`],
+			size === "small" && styles["button--small"],
+			disabled && styles["button--disabled"],
+		)}
+		disabled={disabled}
+		onClick={onClick}
+		type={type}
+	>
+		{label}
+	</button>
+);
 
 export { Button };

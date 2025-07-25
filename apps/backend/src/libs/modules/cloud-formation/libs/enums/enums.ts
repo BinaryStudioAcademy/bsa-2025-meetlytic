ff8d89f8a2c2315a75af58d52892e0b521c5dd00
Volodymyr <Volodumurvel@gmail.com>
2025-07-26 00:08:26 +0200

    refactor: add exception message enum ml-31

---
+++ apps/backend/src/libs/modules/cloud-formation/libs/enums/enums.ts
@@ -0,0 +1,5 @@
+export { Capability } from "./cloudformation-capability.enum.js";
+export { ExceptionMessage } from "./cloudformation-exception-message.enum.js";
+export { OutputKey } from "./cloudformation-output-key.enum.js";
+export { ParameterKey } from "./cloudformation-parameters-key.enum.js";
+export { StackPrefix } from "./cloudformation-stack.enum.js";

3a6be8d51738d57d5d0d16c6cacf2352e25706a7
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-25 14:59:41 +0300

    refactor: remove hardcoded icon name type ml-40

--- apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
+++ apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
@@ -1,15 +1,11 @@
 import ArrowRight from "~/assets/img/icons/arrow-right.svg?react";
 import Meetings from "~/assets/img/icons/meetings.svg?react";
 import Settings from "~/assets/img/icons/settings.svg?react";
-import { type IconName } from "~/libs/types/types.js";
 
-const iconNameToSvg: Record<
-	IconName,
-	React.FC<React.SVGProps<SVGSVGElement>>
-> = {
+const iconNameToSvg = {
 	arrowRight: ArrowRight,
 	meetings: Meetings,
 	settings: Settings,
-};
+} as const;
 
 export { iconNameToSvg };

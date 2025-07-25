36c953fa65a1ec5c93cf81333efe9a87fbe122f9
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-25 19:16:22 +0300

    refactor: return hardcoded icon names ml-40

--- apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
+++ apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
@@ -1,11 +1,15 @@
 import ArrowRight from "~/assets/img/icons/arrow-right.svg?react";
 import Meetings from "~/assets/img/icons/meetings.svg?react";
 import Settings from "~/assets/img/icons/settings.svg?react";
+import { type IconName } from "~/libs/types/types.js";
 
-const iconNameToSvg = {
+const iconNameToSvg: Record<
+	IconName,
+	React.FC<React.SVGProps<SVGSVGElement>>
+> = {
 	arrowRight: ArrowRight,
 	meetings: Meetings,
 	settings: Settings,
-} as const;
+};
 
 export { iconNameToSvg };

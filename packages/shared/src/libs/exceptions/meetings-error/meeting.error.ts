60612fec02075e4cd664594f654241be6176d274
Volodymyr <Volodumurvel@gmail.com>
2025-07-25 08:58:31 +0200

    refactor: change meeting error types and 1 arg ml-31

--- packages/shared/src/libs/exceptions/meetings-error/meeting.error.ts
+++ packages/shared/src/libs/exceptions/meetings-error/meeting.error.ts
@@ -3,12 +3,16 @@ import { MeetingStatusMessage } from "../../../modules/meetings/meetings.js";
 import { ValueOf } from "../../types/types.js";
 import { HTTPError } from "../http-error/http-error.exception.js";
 
+type Constructor = {
+	cause?: unknown;
+	message: ValueOf<typeof MeetingStatusMessage>;
+	status: ValueOf<typeof HTTPCode>;
+};
+
 class MeetingError extends HTTPError {
-	public constructor(
-		message: (typeof MeetingStatusMessage)[keyof typeof MeetingStatusMessage],
-		status: ValueOf<typeof HTTPCode>,
-	) {
+	public constructor({ cause, message, status }: Constructor) {
 		super({
+			cause,
 			message,
 			status,
 		});

d48f4926e66dba6d792ad37c91e3a3776bf2313c
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-25 15:01:58 +0300

    refactor: always add padding if icon present ml-40

---
+++ apps/frontend/src/libs/components/input/libs/maps/input-padding-to-class.map.ts
@@ -0,0 +1,8 @@
+import styles from "../../styles.module.css";
+
+const inputPaddingToClass = {
+	left: styles["input--left-padding"],
+	right: styles["input--right-padding"],
+};
+
+export { inputPaddingToClass };
---
+++ apps/frontend/src/libs/components/input/libs/maps/maps.ts
@@ -0,0 +1,2 @@
+export { iconPositionToClass } from "./icon-position-to-class.map.js";
+export { inputPaddingToClass } from "./input-padding-to-class.map.js";

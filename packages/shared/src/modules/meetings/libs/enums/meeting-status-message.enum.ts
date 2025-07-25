7a42519983d358fc905d5f9c4a624fc2ab3a2e6b
Volodymyr <Volodumurvel@gmail.com>
2025-07-26 00:09:19 +0200

    fix: meeting message status ml-31

--- packages/shared/src/modules/meetings/libs/enums/meeting-status-message.enum.ts
+++ packages/shared/src/modules/meetings/libs/enums/meeting-status-message.enum.ts
@@ -1,7 +1,7 @@
 const MeetingStatusMessage = {
 	CANNOT_UPDATE_NON_EXISTENT: "Cannot update non-existent meeting with ID",
 	FORBIDDEN: "Access denied",
-	MEETING_FAILD_TO_CREATE: "Failed to create meeting",
+	MEETING_FAILED_TO_CREATE: "Failed to create a meeting",
 	MEETING_NOT_FOUND: "Meeting not found",
 	UPDATE_FAILED: "Failed to update meeting",
 } as const;

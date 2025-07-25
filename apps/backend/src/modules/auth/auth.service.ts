c28a49e12c6188d6a4430b58c9147f9f00ca6d42
Ivan Lavruk <ilavr8725@gmail.com>
2025-07-25 23:24:54 +0200

    refactor: replace dependency injection with direct import in AuthService ml-12

--- apps/backend/src/modules/auth/auth.service.ts
+++ apps/backend/src/modules/auth/auth.service.ts
@@ -2,28 +2,22 @@ import { AuthError } from "~/libs/exceptions/exceptions.js";
 import { encrypt } from "~/libs/modules/encrypt/encrypt.js";
 import { jwt } from "~/libs/modules/token/token.js";
 
-import { type UserService } from "../users/user.service.js";
 import {
 	type AuthResponseDto,
+	userService,
 	type UserSignInRequestDto,
 	type UserSignUpRequestDto,
 } from "../users/users.js";
 
 class AuthService {
-	private userService: UserService;
-
-	public constructor(userService: UserService) {
-		this.userService = userService;
-	}
-
 	public async signIn(
 		userRequestDto: UserSignInRequestDto,
 	): Promise<AuthResponseDto> {
-		const user = await this.userService.findByEmail(userRequestDto.email);
+		const user = await userService.findByEmail(userRequestDto.email);
 		if (!user) {
 			throw new AuthError();
 		}
-		const credentials = await this.userService.getCredentials(user.id);
+		const credentials = await userService.getCredentials(user.id);
 		if (!credentials) {
 			throw new AuthError();
 		}
@@ -43,7 +37,7 @@ class AuthService {
 	public async signUp(
 		userRequestDto: UserSignUpRequestDto,
 	): Promise<AuthResponseDto> {
-		const user = await this.userService.create(userRequestDto);
+		const user = await userService.create(userRequestDto);
 		const token = await jwt.sign({ userId: user.id });
 
 		return { token, user };

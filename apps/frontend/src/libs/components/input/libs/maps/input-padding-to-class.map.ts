ee85da3e7d14060057c288ac7f0de1f6a888c08f
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-26 17:08:53 +0300

    refactor: remove extra dash in css classes ml-40

--- apps/frontend/src/libs/components/input/libs/maps/input-padding-to-class.map.ts
+++ apps/frontend/src/libs/components/input/libs/maps/input-padding-to-class.map.ts
@@ -1,8 +1,8 @@
 import styles from "../../styles.module.css";
 
 const inputPaddingToClass = {
-	left: styles["input--left-padding"],
-	right: styles["input--right-padding"],
+	left: styles["input-left-padding"],
+	right: styles["input-right-padding"],
 };
 
 export { inputPaddingToClass };

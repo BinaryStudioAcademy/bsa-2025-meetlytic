bd1d6419e94486633d2c0131cb51847723cf1786
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-07-26 17:02:39 +0300

    refactor: update meetings shared libs ml-29

--- packages/shared/src/modules/meetings/libs/types/meeting-update-request-dto.type.ts
+++ packages/shared/src/modules/meetings/libs/types/meeting-update-request-dto.type.ts
@@ -3,7 +3,6 @@ import { MeetingHost } from "../enums/meetings-host.enum.js";
 
 type MeetingUpdateRequestDto = {
 	host: ValueOf<typeof MeetingHost>;
-	instanceId: null | string;
 	ownerId: number;
 };
 

a628de41254e880c1d635b9b574e65588686d4fe
Volodymyr <Volodumurvel@gmail.com>
2025-07-27 11:26:55 +0200

    feat: add messages failed create , delete ml-31

--- packages/shared/src/libs/enums/exception-message.enum.ts
+++ packages/shared/src/libs/enums/exception-message.enum.ts
@@ -1,5 +1,7 @@
 const ExceptionMessage = {
 	FAILED_TO_GET_INSTANCE: "Failed to get InstanceId from CloudFormation output",
+	FAILED_TO_CREATE_STACK: "Failed to create CloudFormation stack",
+	FAILED_TO_DELETE_STACK: "Failed to delete CloudFormation stack",
 } as const;
 
 export { ExceptionMessage };

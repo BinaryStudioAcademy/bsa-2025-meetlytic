903a6b7d15208a96973b9aba338213ef04a80fdc
Yaroslav <ygroshev@splitdev.com>
2025-07-27 16:15:15 +0300

    refactor(frontend): delete inputs wrappers ml-8

--- apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
@@ -1,6 +1,5 @@
 import { Button, Input, Link } from "~/libs/components/components.js";
-import { AppRoute } from "~/libs/enums/app-route.enum.js";
-import { ButtonVariant } from "~/libs/enums/button-variant.enum.js";
+import { AppRoute, ButtonVariant } from "~/libs/enums/enums.js";
 import { useAppForm, useCallback } from "~/libs/hooks/hooks.js";
 import {
 	type UserSignUpRequestDto,
@@ -32,56 +31,46 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
 		<AuthLayout>
 			<h3 className={styles["title"]}>Create an account</h3>
 			<form className={styles["form"]} onSubmit={handleFormSubmit}>
-				<p className={styles["field"]}>
-					<Input
-						control={control}
-						errors={errors}
-						label="First name"
-						name="firstName"
-						placeholder="First name"
-						type="text"
-					/>
-				</p>
-				<p className={styles["field"]}>
-					<Input
-						control={control}
-						errors={errors}
-						label="Last name"
-						name="lastName"
-						placeholder="Last name"
-						type="text"
-					/>
-				</p>
-				<p className={styles["field"]}>
-					<Input
-						control={control}
-						errors={errors}
-						label="Email"
-						name="email"
-						placeholder="mail@abc.com"
-						type="text"
-					/>
-				</p>
-				<p className={styles["field"]}>
-					<Input
-						control={control}
-						errors={errors}
-						label="Password"
-						name="password"
-						placeholder="*************"
-						type="text"
-					/>
-				</p>
-				<p className={styles["field"]}>
-					<Input
-						control={control}
-						errors={errors}
-						label="Confirm Password"
-						name="confirmPassword"
-						placeholder="*************"
-						type="text"
-					/>
-				</p>
+				<Input
+					control={control}
+					errors={errors}
+					label="First name"
+					name="firstName"
+					placeholder="First name"
+					type="text"
+				/>
+				<Input
+					control={control}
+					errors={errors}
+					label="Last name"
+					name="lastName"
+					placeholder="Last name"
+					type="text"
+				/>
+				<Input
+					control={control}
+					errors={errors}
+					label="Email"
+					name="email"
+					placeholder="mail@abc.com"
+					type="text"
+				/>
+				<Input
+					control={control}
+					errors={errors}
+					label="Password"
+					name="password"
+					placeholder="*************"
+					type="text"
+				/>
+				<Input
+					control={control}
+					errors={errors}
+					label="Confirm Password"
+					name="confirmPassword"
+					placeholder="*************"
+					type="text"
+				/>
 				<Button
 					label="Create an account"
 					type="submit"

bc68283ae1d59bebff1c0dc9421473183e1e6efc
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-07-28 20:35:30 +0300

    refactor: move button wrapping to Input ml-40
    - remove nested conditions
    - center icon with flex
    - remove redundant maps

--- apps/frontend/src/libs/components/icon/icon.tsx
+++ apps/frontend/src/libs/components/icon/icon.tsx
@@ -6,25 +6,12 @@ type Properties = {
 	className?: string | undefined;
 	color?: string | undefined;
 	name: IconName;
-	onClick?: (() => void) | undefined;
 };
 
-const Icon: React.FC<Properties> = ({
-	className,
-	color,
-	name,
-	onClick,
-}: Properties) => {
+const Icon: React.FC<Properties> = ({ className, color, name }: Properties) => {
 	const Icon = iconNameToSvg[name];
-	const isClickable = Boolean(onClick);
 
-	return isClickable ? (
-		<button type="button">
-			<Icon className={className} color={color} onClick={onClick} />
-		</button>
-	) : (
-		<Icon className={className} color={color} />
-	);
+	return <Icon className={className} color={color} />;
 };
 
 export { Icon };
--- apps/frontend/src/libs/components/input/libs/maps/maps.ts
+++
@@ -1,2 +0,0 @@
-export { iconPositionToClass } from "./icon-position-to-class.map.js";
-export { inputPaddingToClass } from "./input-padding-to-class.map.js";

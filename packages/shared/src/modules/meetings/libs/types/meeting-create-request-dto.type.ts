17a30d3b5d86888b93719bfb88396ce57525ae48
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-07-28 11:03:08 +0300

    fix: update meeting-create-request-dto ml-29

--- packages/shared/src/modules/meetings/libs/types/meeting-create-request-dto.type.ts
+++ packages/shared/src/modules/meetings/libs/types/meeting-create-request-dto.type.ts
@@ -4,7 +4,6 @@ import { type MeetingHost } from "../enums/enums.js";
 type MeetingCreateRequestDto = {
 	host: ValueOf<typeof MeetingHost>;
 	instanceId: null | string;
-	ownerId: number;
 };
 
 export { type MeetingCreateRequestDto };

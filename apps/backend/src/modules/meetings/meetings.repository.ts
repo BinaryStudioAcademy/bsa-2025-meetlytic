451be5f0cf8ec2cf26fac180c36ce698cb7cb5ae
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-07-30 10:07:46 +0300

    feat: create meetings ml-29 (#46)
    * feat: add meetings table in DatabaseTableNames ml-29
    
    * feat: update server-application ml-29
    
    * feat: update service.type ml-29
    
    * feat: add meetings module ml-29
    
    * feat: add libs in shared for meetings ml-29
    
    * refactor: rename shared module meetings libs ml-29
    
    * feat: add meetings migration ml-29
    
    * feat: add meeting status message ml-29
    
    * feat: update meeting libs ml-29
    
    * feat: update meetings module ml-29
    
    * refactor: update meetings migration ml-29
    
    * fix: update meetings shared libs ml-29
    
    * refactor: update meetings module ml-29
    
    * refactor: update meeting.repository ml-29
    
    * feat: add meetings table in DatabaseTableNames ml-29
    
    * feat: update server-application ml-29
    
    * feat: update service.type ml-29
    
    * feat: add meetings module ml-29
    
    * feat: add libs in shared for meetings ml-29
    
    * refactor: rename shared module meetings libs ml-29
    
    * feat: add meetings migration ml-29
    
    * feat: add meeting status message ml-29
    
    * feat: update meeting libs ml-29
    
    * feat: update meetings module ml-29
    
    * refactor: update meetings migration ml-29
    
    * fix: update meetings shared libs ml-29
    
    * refactor: update meetings module ml-29
    
    * refactor: update meeting.repository ml-29
    
    * fix: update meetings migration ml-29
    
    * chore: update authoriaztion plugin ml-29
    
    * refactor: update meetings shared libs ml-29
    
    * feat: update meeting module ml-29
    
    * fix: update import paths in meetings libs ml-29
    
    * Revert "Merge branch '29-feat-create-meetings' of https://github.com/BinaryStudioAcademy/bsa-2025-meetlytic into 29-feat-create-meetings"
    
    This reverts commit 17a60f73c58627fdafa14825314d7c5ae82f54b1, reversing
    changes made to 679aeb60c9220a531920dbc1ef0737ddd34bb6bd.
    
    * Reapply "Merge branch '29-feat-create-meetings' of https://github.com/BinaryStudioAcademy/bsa-2025-meetlytic into 29-feat-create-meetings"
    
    This reverts commit b7cd4223a7bcbb1958dca926fe9f82c92fdf61be.
    
    * fix: fix all lint errors ml-29
    
    * fix: update meeting migration ml-29
    
    * feat: update create meetings moudle ml-29
    
    * refactor: update meetings shared libs ml-29
    
    * fix: update authorization.plugin ml-29
    
    * refactor: update create meetings module ml-29
    
    * refactor: update meetingErrorMessage ml-29
    
    * fix: update meeting-create-request-dto ml-29
    
    * fix: update authorization.plugin.ts ml-29
    
    * feat: add status field into meetings table ml-29
    
    * feat: update create meetings module ml-29
    
    * fix: rename preHandler field ml-29
    
    * fix: update meetings.controller ml-29
    
    * refactor: update create meetings ml-29
    
    * refactor: update meetings.service ml-29
    
    * fix: update meeting.service ml-29
    
    * refactor: update meeting.service ml-29
    
    * fix: add useNative into meetings table ml-29
    
    * fix: change id type in api options ml-29
    
    * fix: change id type in delete-meeting-options ml-29
    
    * fix: change params types back ml-29
    
    ---------
    
    Co-authored-by: Volodymyr <Volodumurvel@gmail.com>

---
+++ apps/backend/src/modules/meetings/meetings.repository.ts
@@ -0,0 +1,64 @@
+import { DELETE_SUCCESS_THRESHOLD } from "~/libs/constants/constants.js";
+import { type Repository } from "~/libs/types/types.js";
+
+import { MeetingEntity } from "./meetings.entity.js";
+import { type MeetingModel } from "./meetings.model.js";
+
+class MeetingRepository implements Repository<MeetingEntity> {
+	private meetingModel: typeof MeetingModel;
+
+	public constructor(meetingModel: typeof MeetingModel) {
+		this.meetingModel = meetingModel;
+	}
+
+	public async create(entity: MeetingEntity): Promise<MeetingEntity> {
+		const payload = entity.toNewObject();
+		const meeting = await this.meetingModel
+			.query()
+			.insert(payload)
+			.returning("*")
+			.execute();
+
+		return MeetingEntity.initialize(meeting);
+	}
+
+	public async delete(id: number): Promise<boolean> {
+		const meeting = await this.meetingModel.query().deleteById(id);
+
+		return meeting > DELETE_SUCCESS_THRESHOLD;
+	}
+
+	public async find(id: number): Promise<MeetingEntity | null> {
+		const meeting = await this.meetingModel.query().findById(id);
+
+		return meeting ? MeetingEntity.initialize(meeting) : null;
+	}
+
+	public async findAll(): Promise<MeetingEntity[]> {
+		const meetings = await this.meetingModel.query().execute();
+
+		return meetings.map((meeting) => MeetingEntity.initialize(meeting));
+	}
+
+	public async findAllByOwnerId(ownerId: number): Promise<MeetingEntity[]> {
+		const meetings = await this.meetingModel
+			.query()
+			.where({ ownerId })
+			.execute();
+
+		return meetings.map((meeting) => MeetingEntity.initialize(meeting));
+	}
+
+	public async update(
+		id: number,
+		payload: Partial<Record<string, unknown>>,
+	): Promise<MeetingEntity | null> {
+		const meeting = await this.meetingModel
+			.query()
+			.patchAndFetchById(id, payload);
+
+		return MeetingEntity.initialize(meeting);
+	}
+}
+
+export { MeetingRepository };

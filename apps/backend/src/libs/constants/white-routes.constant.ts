6094cc3c9eff1e3e30889b038a38ffa4c8bb65a8
Anastasiia Bielik <ana.stasy0112@gmail.com>
2025-07-31 17:45:15 +0300

    refactor: simplify white routes for Swagger UI using regex ml-72

--- apps/backend/src/libs/constants/white-routes.constant.ts
+++ apps/backend/src/libs/constants/white-routes.constant.ts
@@ -6,19 +6,7 @@ const WHITE_ROUTES: WhiteRoute[] = [
 	{ method: "POST", path: `${APIPath.AUTH}${AuthApiPath.SIGN_UP}` },
 	{ method: "POST", path: `${APIPath.AUTH}${AuthApiPath.SIGN_IN}` },
 
-	{ method: "GET", path: "/documentation" },
-	{ method: "GET", path: "/documentation/static/swagger-ui.css" },
-	{ method: "GET", path: "/documentation/static/index.css" },
-	{ method: "GET", path: "/documentation/static/swagger-ui-bundle.js" },
-	{
-		method: "GET",
-		path: "/documentation/static/swagger-ui-standalone-preset.js",
-	},
-	{
-		method: "GET",
-		path: "/documentation/static/swagger-initializer.js",
-	},
-	{ method: "GET", path: "/documentation/json" },
+	{ method: "GET", path: "/documentation*" },
 ];
 
 export { WHITE_ROUTES };

4a455f60d088c52f2081bc3580321bd80015b518
Anastasiia Bielik <ana.stasy0112@gmail.com>
2025-07-31 13:41:29 +0300

    refactor: add swagger paths to white routes ml-72

--- apps/backend/src/libs/constants/white-routes.constant.ts
+++ apps/backend/src/libs/constants/white-routes.constant.ts
@@ -5,6 +5,20 @@ import { type WhiteRoute } from "./libs/types/types.js";
 const WHITE_ROUTES: WhiteRoute[] = [
 	{ method: "POST", path: `${APIPath.AUTH}${AuthApiPath.SIGN_UP}` },
 	{ method: "POST", path: `${APIPath.AUTH}${AuthApiPath.SIGN_IN}` },
+
+	{ method: "GET", path: "/documentation" },
+	{ method: "GET", path: "/documentation/static/swagger-ui.css" },
+	{ method: "GET", path: "/documentation/static/index.css" },
+	{ method: "GET", path: "/documentation/static/swagger-ui-bundle.js" },
+	{
+		method: "GET",
+		path: "/documentation/static/swagger-ui-standalone-preset.js",
+	},
+	{
+		method: "GET",
+		path: "/documentation/static/swagger-initializer.js",
+	},
+	{ method: "GET", path: "/documentation/json" },
 ];
 
 export { WHITE_ROUTES };

56480361c9379cc8def0bb7095504a714c05ea6f
Roman-PRV <prokopenkorv@gmail.com>
2025-07-31 14:24:35 +0200

    fix: activate validation.body.parse in the BaseController ml-74

--- apps/backend/src/libs/modules/controller/base-controller.module.ts
+++ apps/backend/src/libs/modules/controller/base-controller.module.ts
@@ -1,5 +1,7 @@
 import { type FastifyReply, type FastifyRequest } from "fastify";
 
+import { ValidationError } from "~/libs/exceptions/exceptions.js";
+import { HTTPCode } from "~/libs/modules/http/http.js";
 import { type Logger } from "~/libs/modules/logger/logger.js";
 import { type ServerApplicationRouteParameters } from "~/libs/modules/server-application/server-application.js";
 
@@ -23,12 +25,28 @@ class BaseController implements Controller {
 		this.routes = [];
 	}
 
-	private async mapHandler(
-		handler: APIHandler,
-		request: FastifyRequest,
-		reply: FastifyReply,
-	): Promise<void> {
+	private async mapHandler(options: {
+		handler: APIHandler;
+		reply: FastifyReply;
+		request: FastifyRequest;
+		validation?: ControllerRouteParameters["validation"];
+	}): Promise<void> {
+		const { handler, reply, request, validation } = options;
 		this.logger.info(`${request.method.toUpperCase()} on ${request.url}`);
+
+		try {
+			if (validation?.body) {
+				request.body = await validation.body.parseAsync(request.body);
+			}
+		} catch (error) {
+			if (error instanceof ValidationError) {
+				return await reply
+					.status(HTTPCode.BAD_REQUEST)
+					.send({ errors: error.errors });
+			}
+
+			throw error;
+		}
 
 		const handlerOptions = this.mapRequest(request);
 		const { payload, status } = await handler(handlerOptions);
@@ -47,12 +65,13 @@ class BaseController implements Controller {
 	}
 
 	public addRoute(options: ControllerRouteParameters): void {
-		const { handler, path, preHandlers } = options;
+		const { handler, path, preHandlers, validation } = options;
 		const fullPath = this.apiUrl + path;
 
 		this.routes.push({
 			...options,
-			handler: (request, reply) => this.mapHandler(handler, request, reply),
+			handler: (request, reply) =>
+				this.mapHandler({ handler, reply, request, validation }),
 			path: fullPath,
 			preHandlers: preHandlers,
 		});

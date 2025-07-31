6768efac57243b5749b70448983102764ee08963
Anastasiia Bielik <ana.stasy0112@gmail.com>
2025-07-31 17:46:26 +0300

    refactor: change route for swagger in white routes ml-72

--- apps/backend/src/libs/constants/white-routes.constant.ts
+++ apps/backend/src/libs/constants/white-routes.constant.ts
@@ -6,7 +6,7 @@ const WHITE_ROUTES: WhiteRoute[] = [
 	{ method: "POST", path: `${APIPath.AUTH}${AuthApiPath.SIGN_UP}` },
 	{ method: "POST", path: `${APIPath.AUTH}${AuthApiPath.SIGN_IN}` },
 
-	{ method: "GET", path: "/documentation*" },
+	{ method: "GET", path: "/documentation" },
 ];
 
 export { WHITE_ROUTES };

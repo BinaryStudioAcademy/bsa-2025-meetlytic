6855a3914692f3ef1670650834644508fa2bb585
Volodymyr <Volodumurvel@gmail.com>
2025-08-01 09:11:08 +0200

    feat: add enums ui label ml-43

---
+++ apps/bot/src/libs/enums/zoom-ui-label.enum.ts
@@ -0,0 +1,15 @@
+const ZoomUiLabel = {
+	CONFIRM_LEAVE: "button.leave-meeting-options__btn--danger",
+	INPUT_NAME: "#input-for-name",
+	JOIN: "button[type='button']",
+	LEAVE: "button[aria-label='Leave']",
+	MUTE: "button[aria-label='mute my microphone']",
+	MUTE_LOGIN: "button[aria-label='Mute']",
+	PARTISIPANTS_COUNT: ".footer-button__number-counter span",
+	SPINNER: "button .spinner",
+	STOP_VIDEO: "button[aria-label='stop my video']",
+	STOP_VIDEO_LOGIN: "button[aria-label='Stop Video']",
+	UNMUTE: "button[aria-label='unmute my microphone']",
+} as const;
+
+export { ZoomUiLabel };

9cfcd30e853620073dfa18f12e1303019be87cad
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-08-02 18:53:54 +0300

    refactor: add dummy usage of openai module ml-45

--- apps/bot/src/libs/modules/open-ai/open-ai.ts
+++ apps/bot/src/libs/modules/open-ai/open-ai.ts
@@ -1,7 +1,8 @@
 import OpenAI from "openai";
 
-import { config } from "../config/config.js";
-import { logger } from "../logger/logger.js";
+import { config } from "~/libs/modules/config/config.js";
+import { logger } from "~/libs/modules/logger/logger.js";
+
 import { BaseOpenAI } from "./base-open-ai.module.js";
 
 const client = new OpenAI({ apiKey: config.ENV.OPEN_AI.OPEN_AI_KEY });

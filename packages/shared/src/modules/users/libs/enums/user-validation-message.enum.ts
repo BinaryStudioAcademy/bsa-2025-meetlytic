b6733db6cc43baf4e2c2ba78c2e1a45cfdcec6b1
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-08-04 20:04:56 +0300

    fix: make email validation message more clear ml-100

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -1,8 +1,8 @@
 import { UserValidationRule } from "./user-validation-rule.enum.js";
 
 const UserValidationMessage = {
+	EMAIL_INVALID: "Please enter a valid email address",
 	EMAIL_REQUIRE: "Email is required",
-	EMAIL_WRONG: "Email is wrong",
 	FIRST_NAME_MAX_LENGTH: `First name must not exceed ${String(UserValidationRule.FIRST_NAME_MAXIMUM_LENGTH)} characters`,
 	FIRST_NAME_MIN_LENGTH: `First name must be at least ${String(UserValidationRule.FIRST_NAME_MINIMUM_LENGTH)} character long`,
 	FIRST_NAME_REQUIRE: "First name is required",

dcee5f82fe247676091db1ff5ba211b0994821b8
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-08-06 19:31:26 +0300

    fix: use usr/bin to install deps ml-44

--- apps/bot/setup-ec2.sh
+++ apps/bot/setup-ec2.sh
@@ -37,24 +37,18 @@ pactl unload-module module-loopback || true
 pactl load-module module-null-sink sink_name=virtual_sink sink_properties=device.description=Virtual_Sink
 pactl load-module module-loopback source=virtual_sink.monitor
 
-echo "[+] Installing bot dependencies..."
-cd /home/ubuntu/bsa-2025-meetlytic/apps/bot
-npm install
 
-echo "[+] Building shared package..."
-cd ../../packages/shared
-npm install
-npm run build
-
-echo "[+] Go back to apps/bot..."
-cd ../../apps/bot
-
-echo "[+] Installing Chromium for Puppeteer (as root)..."
+echo "[+] Install NPM Dependencies & Puppeteer Browser"
+# current /home/ubuntu/bsa-2025-meetlytic
+/usr/bin/npm install
 /usr/bin/npx puppeteer browsers install chrome
 
 echo "[+] Creating audio output directory..."
 mkdir -p /home/ubuntu/audio
 sudo chown -R ubuntu:ubuntu /home/ubuntu/audio
+
+echo "[*] go to apps/bot"
+cd apps/bot
 
 echo "[+] Writing environment variables..."
 cat <<EOF > .env
@@ -70,6 +64,6 @@ OUTPUT_DIRECTORY=/home/ubuntu/audio
 EOF
 
 echo "[+] Starting ZoomBot as user ubuntu..."
-sudo -u ubuntu nohup npm run start:dev > /home/ubuntu/bot.log 2>&1 &
+nohup /usr/bin/npm run start:dev > /home/ubuntu/bot.log 2>&1 &
 
 echo "[\0_0/] Setup complete. Bot is running in background."

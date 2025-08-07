803bef1b8bb7fa030f8fbdd424445c0be7a957d2
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-08-07 13:04:18 +0300

    chore(frontend, shared): move dayjs to shared ml-102

--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,5 +1,4 @@
 export { debounce } from "./debounce.helper.js";
 export { extractErrorMessage } from "./extract-error-message.helper.js";
-export { formatDate } from "./format-date.js";
 export { getValidClassNames } from "./get-valid-class-names.helper.js";
-export { configureString } from "@meetlytic/shared";
+export { configureString, formatDate } from "@meetlytic/shared";

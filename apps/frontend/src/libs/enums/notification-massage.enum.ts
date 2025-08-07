21a32bf13632318686b8421bd737b4ae4f84c328
Egor Kostiuk <kostukegor729@gmail.com>
2025-08-07 11:53:18 +0300

    refactor(shared/frontend): fix user notification messages ml-56

--- apps/frontend/src/libs/enums/notification-massage.enum.ts
+++ apps/frontend/src/libs/enums/notification-massage.enum.ts
@@ -1,7 +1,7 @@
 const NotificationMessage = {
 	AUTH_FAILED: "Authentication failed. Please log in again.",
 	EDIT_PROFILE_FAILED: "Failed to edit profile",
-	EDIT_PROFILE_SUCCESS: "Profile edited",
+	EDIT_PROFILE_SUCCESS: "Profile was edited successfully",
 	SIGN_IN_FAILED:
 		"We couldn't sign you in. Please check your email and password.",
 	SIGN_UP_FAILED: "Something went wrong during sign up. Please try again.",
--- packages/shared/src/modules/users/libs/enums/user-error-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-error-message.enum.ts
@@ -1,6 +1,7 @@
 const UserErrorMessage = {
-	USER_EMAIL_IN_USE: "User email already in use",
-	USER_NOT_FOUND: "User not found",
+	USER_EMAIL_IN_USE:
+		"This email address is already associated with another account.",
+	USER_NOT_FOUND: "We couldn't find an account with this email address.",
 } as const;
 
 export { UserErrorMessage };

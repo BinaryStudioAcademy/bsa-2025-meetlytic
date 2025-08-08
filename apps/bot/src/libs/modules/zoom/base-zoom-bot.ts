9ac6fe07bd4ddf32880890b54b11ccccdf878bf6
Illya Koroliuk <145700600+MrKopchick@users.noreply.github.com>
2025-08-08 10:08:03 +0300

    refactor: mvp hell ml-44

--- apps/bot/src/libs/modules/zoom/base-zoom-bot.ts
+++ apps/bot/src/libs/modules/zoom/base-zoom-bot.ts
@@ -96,7 +96,7 @@ class ZoomBot {
 		}
 
 		try {
-			await this.clickHelper(ZoomUILabel.ACCEPT_COOKIES, TIMEOUTS.FIVE_SECONDS);
+			await this.clickHelper(ZoomUILabel.ACCEPT_COOKIES, TIMEOUTS.ONE_SECOND);
 			this.logger.info(ZoomBotMessages.COOKIES_ACCEPTED);
 		} catch (error) {
 			this.logger.error(
@@ -106,7 +106,7 @@ class ZoomBot {
 
 		try {
 			await this.page.waitForSelector(ZoomUILabel.ACCEPT_TERMS, {
-				timeout: TIMEOUTS.FIVE_SECONDS,
+				timeout: TIMEOUTS.ONE_SECOND,
 				visible: true,
 			});
 
@@ -199,12 +199,13 @@ class ZoomBot {
 			this.logger.info(ZoomBotMessages.JOINED_MEETING);
 			this.audioRecorder.start();
 			this.logger.info(ZoomBotMessages.AUDIO_RECORDING_STARTED);
-			await this.monitorParticipants();
 		} catch (error) {
 			this.logger.error(
 				`${ZoomBotMessages.FAILED_TO_JOIN_MEETING} ${error instanceof Error ? error.message : String(error)}`,
 			);
 		} finally {
+			this.audioRecorder.stop();
+			this.logger.info(ZoomBotMessages.AUDIO_RECORDING_STOPPED);
 			await this.browser?.close();
 		}
 	}

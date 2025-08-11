71b41c9f92028772183682f2a6ab89ead1318ec8
Roman Prokopenko <149939372+Roman-PRV@users.noreply.github.com>
2025-08-11 18:02:48 +0200

    feat: create player track component ml-62 (#135)
    * feat(frontend): create player track ml-62
    
    * feat(frontend): add pause and play icons ml-62
    
    * refactor(frontend): replace key literals with enum ml-62
    
    * refactor(frontend): replace div with button to improve semantics ml-62
    
    * refactor(frontend): remove unused captions track from audio ml-62
    
    * refactor(frontend): use date/time format helper from 102 branch ml-62
    
    * refactor(frontend): simplify classname  ml-62
    
    * refactor(frontend): create explicit format type for date ml-62
    
    * fix(frontend): remove duplicate helper DateFormat ml-62
    
    * feat(frontend): add a temporary page to demonstrate the component ml-62
    
    * refactor(frontend): remove unnecessary getValidClassNames ml-62
    
    * feat(frontend): move the playertrack to meeteng screen ml-62
    
    * feat(shared): add common constant file ml-62
    
    * feat(frontend): add audio event enum  ml-62
    
    * refactor(frontend): rename togglePlayback ml-62
    
    * refactor(frontend): rename LOADEDMETADATA to LOADED_METADATA ml-62
    
    * refactor(shared): adjust import for shared components ml-62
    
    * feat(frontend): create MeetingDetails page to insert PlayerTrack component ml-62
    
    * refactor(frontend): change route names to match branch 131 ml-62
    
    * refactor(frontend): change route names according to the mentor's requirements ml-62
    
    * refactor(frontend): change ValueOf importe ml-62
    
    * fix(frontend): restore version in backend/package.json ml-62
    
    * refactor(frontend): change Icon import path ml-62
    
    * refactor(frontend): use configureString ml-62
    
    * refactor(frontend): replace div with span ml-62
    
    * refactor(frontend): change AppRoute import path ml-62

--- apps/frontend/src/libs/enums/app-route.enum.ts
+++ apps/frontend/src/libs/enums/app-route.enum.ts
@@ -1,6 +1,7 @@
 const AppRoute = {
 	ANY: "*",
 	MEETINGS: "/meetings",
+	MEETINGS_$ID: "/meetings/:id",
 	ROOT: "/",
 	SIGN_IN: "/sign-in",
 	SIGN_UP: "/sign-up",
--- apps/frontend/src/pages/meetings/components/meeting-item/meeting-item.tsx
+++ apps/frontend/src/pages/meetings/components/meeting-item/meeting-item.tsx
@@ -1,29 +1,42 @@
 import PlaceholderAvatar from "~/assets/img/meeting-placeholder.svg";
+import { Link } from "~/libs/components/components.js";
+import { AppRoute } from "~/libs/enums/enums.js";
+import { configureString } from "~/libs/helpers/helpers.js";
+import { type ValueOf } from "~/libs/types/types.js";
 
 import styles from "./styles.module.css";
 
 type Properties = {
 	date: string;
+	id: number;
 	src?: string;
 	title: string;
 };
 
 const MeetingItem: React.FC<Properties> = ({
 	date,
+	id,
 	src,
 	title,
 }: Properties) => {
+	const to = configureString(AppRoute.MEETINGS_$ID, {
+		id: String(id),
+	});
+
 	return (
 		<>
-			<div className={styles["meeting"]}>
-				<div className={styles["meeting__image"]}>
-					<img alt="meeting" src={src ?? PlaceholderAvatar} />
+			<Link to={to as ValueOf<typeof AppRoute>}>
+				<div className={styles["meeting"]}>
+					<div className={styles["meeting__image"]}>
+						<img alt="meeting" src={src ?? PlaceholderAvatar} />
+					</div>
+
+					<div className={styles["meeting__info-wrapper"]}>
+						<h5 className={styles["meeting__title"]}>{title}</h5>
+						<span className={styles["meeting__extra-info"]}>{date}</span>
+					</div>
 				</div>
-				<div className={styles["meeting__info-wrapper"]}>
-					<h5 className={styles["meeting__title"]}>{title}</h5>
-					<div className={styles["meeting__extra-info"]}>{date}</div>
-				</div>
-			</div>
+			</Link>
 		</>
 	);
 };

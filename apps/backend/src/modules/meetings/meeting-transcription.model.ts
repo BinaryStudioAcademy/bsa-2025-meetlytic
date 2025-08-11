b0c45602b5c587a5a0041f995031aa99d6fe3c6b
Volodymyr <Volodumurvel@gmail.com>
2025-08-11 23:30:22 +0200

    feat(backend): add meeting transcription model ml-108

---
+++ apps/backend/src/modules/meetings/meeting-transcription.model.ts
@@ -0,0 +1,36 @@
+import { type RelationMappings } from "objection";
+
+import {
+	AbstractModel,
+	DatabaseTableName,
+} from "~/libs/modules/database/database.js";
+
+import {
+	MeetingAttributes,
+	MeetingTranscriptionAttributes,
+} from "./libs/enums/enums.js";
+import { MeetingModel } from "./meetings.model.js";
+
+class MeetingTranscriptionModel extends AbstractModel {
+	public chunkText!: string;
+	public meetingId!: number;
+
+	public static get relationMappings(): RelationMappings {
+		return {
+			meeting: {
+				join: {
+					from: `${this.tableName}.${MeetingTranscriptionAttributes.MEETING_REF_ID}`,
+					to: `${DatabaseTableName.MEETINGS}.${MeetingAttributes.ID}`,
+				},
+				modelClass: MeetingModel,
+				relation: this.BelongsToOneRelation,
+			},
+		};
+	}
+
+	public static override get tableName(): string {
+		return DatabaseTableName.MEETING_TRANSCRIPTIONS;
+	}
+}
+
+export { MeetingTranscriptionModel };

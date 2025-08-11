ce4f20c9a07f40ed714c732393345c38d7aa3eb0
Egor Kostiuk <kostukegor729@gmail.com>
2025-08-11 19:27:00 +0300

    refactor(backend): fix HTTP methods imports ml-56

--- apps/backend/src/modules/users/libs/enums/enums.ts
+++ apps/backend/src/modules/users/libs/enums/enums.ts
@@ -1,8 +1,3 @@
 export { ColumnName } from "./column-name.enum.js";
 export { UserAttribute } from "./user-attribute.enum.js";
-export {
-	HTTPMethod,
-	UserError,
-	UserErrorMessage,
-	UsersApiPath,
-} from "@meetlytic/shared";
+export { UserErrorMessage, UsersApiPath } from "@meetlytic/shared";

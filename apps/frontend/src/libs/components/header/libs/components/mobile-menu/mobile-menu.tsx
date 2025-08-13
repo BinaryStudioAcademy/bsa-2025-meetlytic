d6a3c53cb9f3cbe5e41f7b8c51ea21b8341b8194
Roman-PRV <prokopenkorv@gmail.com>
2025-08-13 13:41:33 +0200

    feat(frontend): add mobile menu close and hide arrow in navigation ml-155

--- apps/frontend/src/libs/components/header/libs/components/mobile-menu/mobile-menu.tsx
+++ apps/frontend/src/libs/components/header/libs/components/mobile-menu/mobile-menu.tsx
@@ -24,7 +24,7 @@ const MobileMenu: React.FC<Properties> = ({
 					isOpen ? styles["mobile-menu--open"] : null,
 				)}
 			>
-				<Navigation items={NAVIGATION_ITEMS} />
+				<Navigation items={NAVIGATION_ITEMS} onClick={onClose} />
 
 				<div className={styles["mobile-menu__actions"]}>
 					<Button

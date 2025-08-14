f1cab79c8ac1dcb0fe2111a444230743cffd9682
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-08-14 08:16:47 +0300

    feat(frontend): add stopRecording action ml-160

--- apps/frontend/src/modules/auth/auth-api.ts
+++ apps/frontend/src/modules/auth/auth-api.ts
@@ -1,4 +1,4 @@
-import { APIPath, ContentType } from "~/libs/enums/enums.js";
+import { APIPath, ContentType, HTTPMethod } from "~/libs/enums/enums.js";
 import { BaseHTTPApi } from "~/libs/modules/api/api.js";
 import { type HTTP } from "~/libs/modules/http/http.js";
 import { type Storage } from "~/libs/modules/storage/storage.js";
@@ -28,7 +28,7 @@ class AuthApi extends BaseHTTPApi {
 			{
 				contentType: ContentType.JSON,
 				hasAuth: true,
-				method: "GET",
+				method: HTTPMethod.GET,
 			},
 		);
 
@@ -41,7 +41,7 @@ class AuthApi extends BaseHTTPApi {
 			{
 				contentType: ContentType.JSON,
 				hasAuth: false,
-				method: "POST",
+				method: HTTPMethod.POST,
 				payload: JSON.stringify(payload),
 			},
 		);
@@ -55,7 +55,7 @@ class AuthApi extends BaseHTTPApi {
 			{
 				contentType: ContentType.JSON,
 				hasAuth: false,
-				method: "POST",
+				method: HTTPMethod.POST,
 				payload: JSON.stringify(payload),
 			},
 		);
--- apps/frontend/src/pages/meeting-details/meeting-details.tsx
+++ apps/frontend/src/pages/meeting-details/meeting-details.tsx
@@ -1,12 +1,19 @@
-import { PlayerTrack } from "~/libs/components/components.js";
-import { useParams } from "~/libs/hooks/hooks.js";
+import { Button, PlayerTrack } from "~/libs/components/components.js";
+import { useAppDispatch, useCallback, useParams } from "~/libs/hooks/hooks.js";
+import { actions as meetingActions } from "~/modules/meeting/meeting.js";
 
 const MeetingDetails: React.FC = () => {
+	const dispatch = useAppDispatch();
 	const { id } = useParams<{ id: string }>();
+
+	const stopRecording = useCallback(() => {
+		void dispatch(meetingActions.stopRecording({ id: id as string }));
+	}, [dispatch, id]);
 
 	return (
 		<>
 			<div>Meeting Nº {id}</div>
+			<Button label="Stop Recording" onClick={stopRecording} />
 			<div>
 				<PlayerTrack audioUrl="https://audio-samples.github.io/samples/mp3/wavenet_unconditional/voxceleb2/sample-5.mp3" />
 			</div>

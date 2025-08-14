4c216fd8ff71b44f73663503461594d1ac060ef2
Roman Prokopenko <149939372+Roman-PRV@users.noreply.github.com>
2025-08-14 13:51:11 +0200

    feat: add layout component mobile responsiveness ml-155 (#181)
    * feat(frontend): create draft of mobile layout ml-155
    
    * feat(frontend): add mobile size to avatar ml-155
    
    * feat(frontend): add mobile logo source ml-155
    
    * refactor(frontend): move MobileMenu to its own component ml-155
    
    * feat(frontend): add mobile menu close and hide arrow in navigation ml-155
    
    * refactor(frontend): refactor class names to BEM convention ml-155
    
    * fix(frontend): repair arrows align in NavigationLink ml-155
    
    * fix(frontend): repair logo size ml-155
    
    * fix(frontend): remove unnesasary navigation items ml-155
    
    * fix(frontend): expand backdrop  ml-155
    
    * fix(frontend): remove unnesasary style ml-155
    
    * refactor(frontend): move MobileMenu to Layoute ml-155
    
    * fix(frontend): fix header scrolling ml-155
    
    * chore(frontend): update dependenciesg ml-155
    
    * fix(frontend): limit width of main layout ml-155
    
    * fix(frontend): fix icon names ml-155
    
    * fix(frontend): fix logo-mobile size ml-155
    
    * refactor(frontend): replace handleLogout with onLogout in props ml-155
    
    * refactor(frontend): improve naming and structure of components ml-155
    
    * feat(frontend): add z-indexes to global variables ml-155
    
    * refactor(frontend): add handleGetLogo instead if ml-155
    
    * refactor(frontend): improve naming and structure of components ml-155
    
    * refactor(frontend): improve some styles and naming of components ml-155
    
    * chore(frontend): refresh dependencies ml-155

--- apps/frontend/src/libs/components/navigation/navigation.tsx
+++ apps/frontend/src/libs/components/navigation/navigation.tsx
@@ -5,13 +5,14 @@ import styles from "./styles.module.css";
 
 type Properties = {
 	items: NavigationItem[];
+	onClick?: () => void;
 };
 
-const Navigation: React.FC<Properties> = ({ items }: Properties) => (
+const Navigation: React.FC<Properties> = ({ items, onClick }: Properties) => (
 	<nav>
 		<ul className={styles["navigation-list"]}>
 			{items.map((item) => (
-				<NavigationLink key={item.label} {...item} />
+				<NavigationLink key={item.label} {...item} onClick={onClick} />
 			))}
 		</ul>
 	</nav>

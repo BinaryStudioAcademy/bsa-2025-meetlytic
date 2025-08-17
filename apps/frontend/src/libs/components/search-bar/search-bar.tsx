26aed52eba266ca2cccdf892aaa8b2bbb85f65f2
Volodymyr <Volodumurvel@gmail.com>
2025-08-17 13:34:38 +0200

    feat(frontend): create reusable search bat ml-180

---
+++ apps/frontend/src/libs/components/search-bar/search-bar.tsx
@@ -0,0 +1,36 @@
+import { SearchInput } from "~/libs/components/components.js";
+import { useAppForm } from "~/libs/hooks/hooks.js";
+import { DEFAULT_SEARCH_VALUE } from "~/modules/meeting-details/libs/default-values/meeting-details.default-values.js";
+import { searchInputValidationSchema } from "~/modules/meeting-details/meeting-details.js";
+
+import styles from "./search-bar.module.css";
+
+type Properties = {
+	className?: string;
+	onSearch: (value: string) => void;
+};
+
+const SearchBar: React.FC<Properties> = ({
+	className,
+	onSearch,
+}: Properties) => {
+	const { control, errors } = useAppForm({
+		defaultValues: DEFAULT_SEARCH_VALUE,
+		validationSchema: searchInputValidationSchema,
+	});
+
+	return (
+		<div className={className ?? styles["panel-header__search"]}>
+			<SearchInput
+				control={control}
+				errors={errors}
+				hasVisuallyHiddenLabel={true}
+				label="Search"
+				name="search"
+				onSearch={onSearch}
+			/>
+		</div>
+	);
+};
+
+export { SearchBar };

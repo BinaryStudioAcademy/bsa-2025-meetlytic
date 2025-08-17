bbb6db1c8ce352c4e88c32fa965f223038eccfe9
Yaroslav <188190997+yaroslavgroshev@users.noreply.github.com>
2025-08-17 19:11:10 +0300

    refactor(frontend): delete unused helpers ml-173

--- apps/frontend/src/libs/helpers/extract-error-message.helper.ts
+++ apps/frontend/src/libs/helpers/extract-error-message.helper.ts
@@ -1,53 +1,29 @@
-import { NotificationMessage, ServerErrorType } from "~/libs/enums/enums.js";
-
-type MaybeHTTPError = {
-	details?: unknown;
-	errorType?: unknown;
-	message?: unknown;
-	name?: unknown;
-	status?: unknown;
-};
-
-const isHttpErrorWithResponse = (
-	error: unknown,
-): error is { response: { data?: { message?: unknown } } } =>
-	typeof error === "object" &&
-	error !== null &&
-	"response" in error &&
-	typeof (error as { response: unknown }).response === "object" &&
-	(error as { response: unknown }).response !== null;
-
-const isCustomHttpError = (error: unknown): error is MaybeHTTPError =>
-	typeof error === "object" &&
-	error !== null &&
-	("errorType" in error || (error as { name?: unknown }).name === "HTTPError");
-
 const extractErrorMessage = (
 	error: unknown,
 	fallbackMessage = "Something went wrong",
 ): string => {
-	if (isCustomHttpError(error)) {
-		const error_ = error;
-		const message =
-			typeof error_.message === "string" ? error_.message : undefined;
+	if (typeof error === "object" && error !== null) {
+		const errorMessage = (error as { message?: unknown }).message;
 
-		if (error_.errorType === ServerErrorType.NO_INTERNET) {
-			return message ?? NotificationMessage.NO_INTERNET;
+		if (typeof errorMessage === "string") {
+			return errorMessage;
 		}
 
-		return message ?? fallbackMessage;
-	}
+		const maybeResponse = (error as { response?: unknown }).response;
 
-	if (isHttpErrorWithResponse(error)) {
-		const maybeMessage = (error.response.data as { message?: string }).message;
+		if (
+			typeof maybeResponse === "object" &&
+			maybeResponse !== null &&
+			"data" in maybeResponse &&
+			typeof (maybeResponse as { data?: unknown }).data === "object"
+		) {
+			const maybeMessage = (maybeResponse as { data: { message?: unknown } })
+				.data.message;
 
-		if (typeof maybeMessage === "string" && maybeMessage) {
-			return maybeMessage;
+			if (typeof maybeMessage === "string") {
+				return maybeMessage;
+			}
 		}
-	}
-
-	if (typeof error === "string") {
-		return error;
 	}
 
 	return fallbackMessage;

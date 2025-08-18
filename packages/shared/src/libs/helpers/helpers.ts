223a4a15ea9b295e3497a9733b30ad815b521ea0
Roman-PRV <prokopenkorv@gmail.com>
2025-08-18 20:59:07 +0200

    refactor(backend): rename isPositiveNumber to checkIsValidId ml-222

--- packages/shared/src/libs/helpers/helpers.ts
+++ packages/shared/src/libs/helpers/helpers.ts
@@ -1,5 +1,5 @@
+export { checkIsValidId } from "./check-is-valid-id/check-is-valid-id.js";
 export { configureString } from "./configure-string/configure-string.helper.js";
 export { extractZoomMeetingId } from "./extract-zoom-meeting-id/extract-zoom-meeting-id.helper.js";
 export { formatDate } from "./format-date/format-date.js";
-export { isPositiveNumber } from "./is-positive-number/is-positive-number.js";
 export { isZoomLink } from "./validate-zoom-link/validate-zoom-link.js";

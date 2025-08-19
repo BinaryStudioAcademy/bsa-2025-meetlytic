92925a83838c05d5e23944339a4640ac34b84833
Roman-PRV <prokopenkorv@gmail.com>
2025-08-19 13:24:05 +0200

    fix(backend): change filename for meeting id validation schema ml-222

--- packages/shared/src/modules/meetings/libs/validation-schemas/validation-schemas.ts
+++ packages/shared/src/modules/meetings/libs/validation-schemas/validation-schemas.ts
@@ -1,4 +1,4 @@
 export { meetingCreate } from "./meeting-create.validation-schema.js";
-export { meetingId } from "./meeting-get-by-id.validation-schema.js";
+export { meetingId } from "./meeting-id.validation-schema.js";
 export { meetingUpdate } from "./meeting-update.validation-schema.js";
 export { searchInputValidationSchema } from "./search-input-validation-schema.js";

d3543fd626f6738366b15e1184612f666259fb67
Yaroslav <188190997+yaroslavgroshev@users.noreply.github.com>
2025-08-21 02:50:07 +0300

    refactor(backend): make file repo universal ml-65

--- apps/backend/src/modules/files/file.repository.ts
+++ apps/backend/src/modules/files/file.repository.ts
@@ -1,98 +1,66 @@
-import { type UserDetailsModel } from "~/modules/users/user-details.model.js";
+import { type Transaction } from "objection";
 
 import { type FileModel } from "./file.model.js";
 import { type File } from "./libs/types/types.js";
 
 type Constructor = {
 	fileModel: typeof FileModel;
-	userDetailsModel: typeof UserDetailsModel;
 };
 
-const FILE_TYPE_AVATAR = "avatar";
+type CreateFileParameters = {
+	contentType: string;
+	key: string;
+	url: string;
+};
+
+type Trx = Transaction | undefined;
+
+type UpdateFileParameters = {
+	contentType?: string;
+	key?: string;
+	url?: string;
+};
 
 class FileRepository {
 	private fileModel: typeof FileModel;
-	private userDetailsModel: typeof UserDetailsModel;
 
-	public constructor({ fileModel, userDetailsModel }: Constructor) {
+	public constructor({ fileModel }: Constructor) {
 		this.fileModel = fileModel;
-		this.userDetailsModel = userDetailsModel;
 	}
 
-	public async create(parameters: {
-		contentType: string;
-		key: string;
-		url: string;
-		userDetailsId: number;
-	}): Promise<File> {
-		const { contentType, key, url, userDetailsId } = parameters;
-
+	public async create(
+		parameters: CreateFileParameters,
+		trx?: Trx,
+	): Promise<File> {
 		const fileRow = await this.fileModel
-			.query()
-			.insert({ contentType, key, url })
+			.query(trx)
+			.insert(parameters)
 			.returning("*");
 
-		await this.userDetailsModel
-			.query()
-			.patch({ avatarFileId: fileRow.id })
-			.where("id", userDetailsId);
-
-		return { ...fileRow.toJSON(), type: FILE_TYPE_AVATAR } as File;
+		return fileRow.toJSON() as unknown as File;
 	}
 
-	public async delete(id: number): Promise<number> {
-		return await this.fileModel.query().deleteById(id);
+	public async delete(id: number, trx?: Trx): Promise<number> {
+		return await this.fileModel.query(trx).deleteById(id);
 	}
 
-	public async findById(id: number): Promise<File | null> {
-		const fileRow = await this.fileModel.query().findById(id);
+	public async findById(id: number, trx?: Trx): Promise<File | null> {
+		const fileRow = await this.fileModel.query(trx).findById(id);
 
-		return fileRow
-			? ({ ...fileRow.toJSON(), type: FILE_TYPE_AVATAR } as File)
-			: null;
-	}
-
-	public async findByUserDetailsId(
-		userDetailsId: number,
-	): Promise<File | null> {
-		const userDetails = await this.userDetailsModel
-			.query()
-			.findById(userDetailsId)
-			.select("avatarFileId")
-			.first();
-
-		if (!userDetails?.avatarFileId) {
-			return null;
-		}
-
-		const fileRow = await this.fileModel
-			.query()
-			.findById(userDetails.avatarFileId);
-
-		if (!fileRow) {
-			return null;
-		}
-
-		return { ...fileRow.toJSON(), type: FILE_TYPE_AVATAR } as File;
-	}
-
-	public async unsetFileId(userDetailsId: number): Promise<number> {
-		return await this.userDetailsModel
-			.query()
-			.patch({ avatarFileId: null })
-			.where("id", userDetailsId);
+		return fileRow ? (fileRow.toJSON() as unknown as File) : null;
 	}
 
 	public async update(
 		id: number,
-		parameters: { contentType?: string; key: string; url: string },
+		parameters: UpdateFileParameters,
+		trx?: Trx,
 	): Promise<File> {
-		const updatedRow = await this.fileModel
-			.query()
+		const fileRow = await this.fileModel
+			.query(trx)
 			.patchAndFetchById(id, parameters);
 
-		return { ...updatedRow.toJSON(), type: FILE_TYPE_AVATAR } as File;
+		return fileRow.toJSON() as unknown as File;
 	}
 }
 
-export { FileRepository };
+export { type CreateFileParameters, type Trx, FileRepository };

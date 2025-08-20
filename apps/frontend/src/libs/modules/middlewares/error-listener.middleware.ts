3fd76abbb3588ca4b5ab1faed13f43ec737b9dbd
Egor Kostiuk <kostukegor729@gmail.com>
2025-08-20 13:41:25 +0300

    feat(frontend): add action status check ml-174

--- apps/frontend/src/libs/modules/middlewares/error-listener.middleware.ts
+++ apps/frontend/src/libs/modules/middlewares/error-listener.middleware.ts
@@ -1,7 +1,11 @@
 import { createListenerMiddleware, isRejected } from "@reduxjs/toolkit";
 
+import { HTTPCode } from "~/libs/modules/http/http.js";
 import { notification } from "~/libs/modules/notifications/notifications.js";
-import { type AsyncThunkConfig } from "~/libs/types/types.js";
+import {
+	type AsyncThunkConfig,
+	type RejectPayload,
+} from "~/libs/types/types.js";
 import { actions as authActions } from "~/modules/auth/auth.js";
 
 const errorListenerMiddleware = createListenerMiddleware<
@@ -11,16 +15,14 @@ const errorListenerMiddleware = createListenerMiddleware<
 
 errorListenerMiddleware.startListening({
 	effect: async (action, listenerApi) => {
-		const message =
-			typeof action.payload === "string"
-				? action.payload
-				: (action.error.message ?? "Something went wrong");
+		const { message, status } = action.payload as RejectPayload;
 
-		notification.error(message);
-
-		if (action.error.message === "Invalid credentials") {
+		if (status === HTTPCode.UNAUTHORIZED) {
+			notification.error(message ?? "Session expired. Please log in again.");
 			await listenerApi.dispatch(authActions.logout());
 		}
+
+		notification.error(message ?? "Something went wrong");
 	},
 	predicate: isRejected,
 });

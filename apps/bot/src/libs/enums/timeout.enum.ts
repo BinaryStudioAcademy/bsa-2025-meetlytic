63fe8080ced9888a1291130ffd30b0830e9570af
Volodymyr <Volodumurvel@gmail.com>
2025-08-20 15:07:19 +0200

    chore(bot): delete unused var ml-232

--- apps/bot/src/libs/enums/timeout.enum.ts
+++ apps/bot/src/libs/enums/timeout.enum.ts
@@ -1,11 +1,9 @@
 const Timeout = {
 	FIFTEEN_SECONDS: 15_000,
-
 	FIVE_SECONDS: 5000,
 	ONE_SECOND: 1000,
 	SIXTEEN_SECONDS: 60_000,
 	TEN_SECONDS: 10_000,
-	THREE_SECONDS: 3000,
 } as const;
 
 export { Timeout };

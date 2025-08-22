69ba432f1750e23c37c669755db05f8d79705f57
Volodymyr <Volodumurvel@gmail.com>
2025-08-22 19:02:22 +0200

    feat(frontend): create live transcription component ml-243

---
+++ apps/frontend/src/libs/components/transcription-panel/components/live-transcription/live-transcription.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { memo, TypingIndicator } from "~/libs/components/components.js";
+import { IndicatorVariant } from "~/libs/components/typing-indicator/typing-indicator.js";
+
+type Properties = {
+	isTyping: boolean;
+	typedText: string;
+};
+
+const LiveTranscription: React.FC<Properties> = memo(
+	({ isTyping, typedText }: Properties) => {
+		return (
+			<>
+				{typedText}
+				{!isTyping && (
+					<>
+						<TypingIndicator variant={IndicatorVariant.DOT} />
+					</>
+				)}
+			</>
+		);
+	},
+);
+
+LiveTranscription.displayName = "LiveTranscription";
+
+export { LiveTranscription };

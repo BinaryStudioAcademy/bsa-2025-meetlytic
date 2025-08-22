e6829becf8c1d36d94cc13bae4a0fce9c1daa3d6
Volodymyr <Volodumurvel@gmail.com>
2025-08-22 20:41:05 +0200

    fix(frontend): fix styles, imports ml-243

--- apps/frontend/src/libs/components/typing-indicator/typing-indicator.tsx
+++ apps/frontend/src/libs/components/typing-indicator/typing-indicator.tsx
@@ -1,10 +1,10 @@
-import { getValidClassNames } from "~/libs/helpers/get-valid-class-names.helper.js";
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import { type ValueOf } from "~/libs/types/types.js";
 
-import { IndicatorVariant } from "./libs/enums/indicator-variant.enum.js";
+import { IndicatorVariant } from "./libs/enums/enums.js";
 import styles from "./typing-indicator.module.css";
 
-const SIZE_OF_DOT = 4;
+const DOT_INDICATOR_DEFAULT_SIZE = 4;
 
 type Properties = {
 	color?: string;
@@ -14,7 +14,7 @@ type Properties = {
 
 const TypingIndicator: React.FC<Properties> = ({
 	color = "currentColor",
-	size = SIZE_OF_DOT,
+	size = DOT_INDICATOR_DEFAULT_SIZE,
 	variant = IndicatorVariant.DOT,
 }: Properties) => {
 	const style = { backgroundColor: color, height: size, width: size };
@@ -26,9 +26,9 @@ const TypingIndicator: React.FC<Properties> = ({
 				styles[variant],
 			)}
 		>
-			<span style={style}></span>
-			<span style={style}></span>
-			<span style={style}></span>
+			<span className={styles["typing-indicator__item"]} style={style}></span>
+			<span className={styles["typing-indicator__item"]} style={style}></span>
+			<span className={styles["typing-indicator__item"]} style={style}></span>
 		</span>
 	);
 };

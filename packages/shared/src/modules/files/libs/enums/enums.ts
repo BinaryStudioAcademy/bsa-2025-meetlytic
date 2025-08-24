f347e92cb31f52e176322f6e3d876e7866451732
Yaroslav <188190997+yaroslavgroshev@users.noreply.github.com>
2025-08-24 18:41:38 +0300

    refactor(backend): change filename ml-65

--- packages/shared/src/modules/files/libs/enums/enums.ts
+++ packages/shared/src/modules/files/libs/enums/enums.ts
@@ -1,2 +1,2 @@
 export { FileErrorMessage } from "./file-error-message.enum.js";
-export { UploadErrorMessage } from "./upload-error-message-enum.js";
+export { UploadErrorMessage } from "./upload-error-message.enum.js";

14cbc96ce5f054ef63b5b8c7033a3d22abf0eabc
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-08-26 11:35:31 +0300

    Revert "feat(backend): switch to feat branch ml-300"
    This reverts commit a542d6ae479bf8d7b1ae56df0657450ce40b4537.

--- apps/bot/src/libs/modules/zoom/base-zoom-bot.ts
+++ apps/bot/src/libs/modules/zoom/base-zoom-bot.ts
@@ -274,8 +274,9 @@ class BaseZoomBot {
 
 		await this.clickHelper(ZoomUILabel.MUTE_LOGIN);
 		await this.clickHelper(ZoomUILabel.STOP_VIDEO_LOGIN);
-		await this.enterMeetingPassword();
 		await this.clickHelper(ZoomUILabel.JOIN, Timeout.FIFTEEN_SECONDS);
+		await this.enterMeetingPassword();
+		await this.clickHelper(ZoomUILabel.JOIN, Timeout.TEN_SECONDS);
 	}
 
 	private async leaveMeeting(): Promise<void> {

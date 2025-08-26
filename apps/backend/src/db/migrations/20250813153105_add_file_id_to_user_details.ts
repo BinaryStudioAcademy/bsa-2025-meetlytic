dedde3c0cf42beabf7f5df40028ea8aea4858a3b
Yaroslav <188190997+yaroslavgroshev@users.noreply.github.com>
2025-08-26 04:02:34 +0300

    refactor(backend): change migration file ml-65

--- apps/backend/src/db/migrations/20250813153105_add_file_id_to_user_details.ts
+++ apps/backend/src/db/migrations/20250813153105_add_file_id_to_user_details.ts
@@ -1,53 +1,33 @@
 import { type Knex } from "knex";
 
-import { DatabaseTableName } from "~/libs/modules/database/libs/enums/enums.js";
+const TableName = {
+	FILES: "files",
+	USER_DETAILS: "user_details",
+} as const;
 
 const ColumnName = {
 	AVATAR_FILE_ID: "avatar_file_id",
-	CONTENT_TYPE: "content_type",
-	CREATED_AT: "created_at",
 	ID: "id",
-	KEY: "key",
-	UPDATED_AT: "updated_at",
-	URL: "url",
-	USER_DETAILS_ID: "user_details_id",
 } as const;
 
-const DELETE_SET_NULL = "SET NULL";
+const DELETE_STRATEGY = "SET NULL";
 
 async function down(knex: Knex): Promise<void> {
-	await knex.schema.alterTable(DatabaseTableName.USER_DETAILS, (table) => {
+	await knex.schema.alterTable(TableName.USER_DETAILS, (table) => {
 		table.dropColumn(ColumnName.AVATAR_FILE_ID);
 	});
-
-	await knex.schema.dropTableIfExists(DatabaseTableName.FILES);
 }
 
 async function up(knex: Knex): Promise<void> {
-	await knex.schema.createTable(DatabaseTableName.FILES, (table) => {
-		table.increments(ColumnName.ID).primary();
-		table.string(ColumnName.CONTENT_TYPE).notNullable();
-		table.string(ColumnName.KEY).notNullable();
-		table.string(ColumnName.URL).notNullable();
-		table
-			.timestamp(ColumnName.CREATED_AT)
-			.defaultTo(knex.fn.now())
-			.notNullable();
-		table
-			.timestamp(ColumnName.UPDATED_AT)
-			.defaultTo(knex.fn.now())
-			.notNullable();
-	});
-
-	await knex.schema.alterTable(DatabaseTableName.USER_DETAILS, (table) => {
+	await knex.schema.alterTable(TableName.USER_DETAILS, (table) => {
 		table
 			.integer(ColumnName.AVATAR_FILE_ID)
 			.unsigned()
 			.nullable()
 			.references(ColumnName.ID)
-			.inTable(DatabaseTableName.FILES)
-			.onDelete(DELETE_SET_NULL);
-		table.index(ColumnName.AVATAR_FILE_ID);
+			.inTable(TableName.FILES)
+			.onDelete(DELETE_STRATEGY)
+			.index();
 	});
 }
 

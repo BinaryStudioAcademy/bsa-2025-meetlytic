9661bd5ff45db773dfa77a38068b3832bc4b2ae3
Serhii Ulianchenko <ulianchenko@gmail.com>
2025-08-26 22:58:03 +0300

    fix: change variable naming ml-206

--- packages/shared/src/modules/meetings/libs/validation-schemas/meeting-id-route-parameter.validation-schema.ts
+++ packages/shared/src/modules/meetings/libs/validation-schemas/meeting-id-route-parameter.validation-schema.ts
@@ -8,12 +8,12 @@ import {
 const meetingIdRouteParameter = z.object({
 	id: z.coerce
 		.number({
-			invalid_type_error: MeetingValidationMessage.MMETING_ID_MUST_BE_NUMBER,
+			invalid_type_error: MeetingValidationMessage.MEETING_ID_MUST_BE_NUMBER,
 		})
-		.int(MeetingValidationMessage.MMETING_ID_MUST_BE_INTEGER)
-		.positive(MeetingValidationMessage.MMETING_ID_MUST_BE_POSITIVE)
+		.int(MeetingValidationMessage.MEETING_ID_MUST_BE_INTEGER)
+		.positive(MeetingValidationMessage.MEETING_ID_MUST_BE_POSITIVE)
 		.max(MeetingValidationRule.MEETING_ID_MAXIMUM_NUMBER, {
-			message: MeetingValidationMessage.MMETING_ID_TOO_LARGE,
+			message: MeetingValidationMessage.MEETING_ID_TOO_LARGE,
 		}),
 });
 

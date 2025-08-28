44b38690762d757b0732e94e903b31c261dca2b5
Roman Prokopenko <149939372+Roman-PRV@users.noreply.github.com>
2025-08-28 10:06:12 +0200

    fix: convert markdown to styles for pdf export ml-301 (#328)
    * chore: refresh-dependencies ml-301
    
    * feat(frontend): disable the export button until the end of the meeting ml-301
    
    * feat(frontend): add render markdown ml-301
    
    * refactor(frontend): beautify imports ml-301
    
    * refactor(frontend): organize the folder structure ml-301
    
    * refactor(frontend): remove unnesasery button status ml-301
    
    * fix(frontend): chore: update dependencies ml-301
    
    * refactor(backend): rename astLina ml-339
    
    * refactor(frontend): improve code ml-339
    
    * refactor(frontend): use arrow functions ml-339

--- apps/frontend/src/libs/constants/constants.ts
+++ apps/frontend/src/libs/constants/constants.ts
@@ -1,5 +1,6 @@
 export { ACCEPTED_IMAGE_TYPES } from "./avatar-types.constant.js";
 export { CREATE_MEETING_FORM_DEFAULT_VALUES } from "./create-meeting-form-default-values.constant.js";
+export { HEADING_LEVEL_PRIMARY } from "./heading-level-primary.js";
 export { NAVIGATION_ITEMS } from "./navigation-items.constant.js";
 export {
 	EMPTY_ARRAY_LENGTH,

4c0d9e3165a08cac8583db541ac121f938ce9c0d
Yaroslav <188190997+yaroslavgroshev@users.noreply.github.com>
2025-08-28 09:35:38 +0300

    Merge branch 'main' into 65-feat-add-user-avatar

---
+++ apps/backend/src/db/migrations/20250818155648_add_new_fields_to_meeting_status.ts
@@ -0,0 +1,59 @@
+import { type Knex } from "knex";
+
+const TableName = {
+	MEETINGS: "meetings",
+} as const;
+
+const ColumnName = {
+	STATUS: "status",
+} as const;
+
+const ENUM_NAME = "meeting_status";
+
+const MeetingStatus = {
+	ENDED: "ended",
+	FAILED: "failed",
+	JOINING: "joining",
+	RECORDING: "recording",
+	STARTED: "started",
+} as const;
+
+async function down(knex: Knex): Promise<void> {
+	await knex.transaction(async (trx) => {
+		await trx(TableName.MEETINGS)
+			.where(ColumnName.STATUS, MeetingStatus.FAILED)
+			.update({ [ColumnName.STATUS]: MeetingStatus.ENDED });
+
+		await trx(TableName.MEETINGS)
+			.whereIn(ColumnName.STATUS, [
+				MeetingStatus.JOINING,
+				MeetingStatus.RECORDING,
+			])
+			.update({ [ColumnName.STATUS]: MeetingStatus.STARTED });
+
+		const temporary = `${ENUM_NAME}_old`;
+
+		await trx.raw(`ALTER TYPE "${ENUM_NAME}" RENAME TO "${temporary}";`);
+		await trx.raw(
+			`CREATE TYPE "${ENUM_NAME}" AS ENUM ('${MeetingStatus.ENDED}', '${MeetingStatus.STARTED}');`,
+		);
+		await trx.raw(
+			` ALTER TABLE "${TableName.MEETINGS}" ALTER COLUMN "${ColumnName.STATUS}" TYPE "${ENUM_NAME}" USING "${ColumnName.STATUS}"::text::"${ENUM_NAME}";`,
+		);
+		await trx.raw(`DROP TYPE "${temporary}";`);
+	});
+}
+
+async function up(knex: Knex): Promise<void> {
+	await knex.raw(
+		`ALTER TYPE "${ENUM_NAME}" ADD VALUE IF NOT EXISTS '${MeetingStatus.FAILED}';`,
+	);
+	await knex.raw(
+		`ALTER TYPE "${ENUM_NAME}" ADD VALUE IF NOT EXISTS '${MeetingStatus.JOINING}';`,
+	);
+	await knex.raw(
+		`ALTER TYPE "${ENUM_NAME}" ADD VALUE IF NOT EXISTS '${MeetingStatus.RECORDING}';`,
+	);
+}
+
+export { down, up };
